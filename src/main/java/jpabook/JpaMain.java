fa4c29de0d544f04bbcd216e1fddc6e15d0de8cf
hyogeon <men1219@hanmail.net>
2020-03-20 05:38:56 +0900

    feat: relational mapping

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,6 +16,8 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
